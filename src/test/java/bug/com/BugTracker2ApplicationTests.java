2ff1501f334972eb64d4c69e26bd7508876a5091
Agusia <aga.gasiewicz@gmail.com>
2021-06-10 21:06:44 +0200

    projekt

--- src/test/java/bug/com/BugTracker2ApplicationTests.java
+++ src/test/java/bug/com/BugTracker2ApplicationTests.java
@@ -1,13 +1,11 @@
 package bug.com;
 
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@ExtendWith(SpringExtension.class)
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+
 @SpringBootTest
 class BugTracker2ApplicationTests {
 
--- src/test/java/bug/com/project/ProjectServiceTest.java
+++ src/test/java/bug/com/project/ProjectServiceTest.java
@@ -1,10 +1,19 @@
 package bug.com.project;
 
+import bug.com.auth.Person;
+import bug.com.issues.Issue;
+import bug.com.issues.IssueService;
+import org.junit.Ignore;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.access.method.P;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -15,10 +24,12 @@ public class ProjectServiceTest {
 
     @Mock
     private ProjectRepository projectRepository;
-//    @Mock
-//    Project project;
+    @Mock
+    private IssueService issueService;
+
     @InjectMocks
     private ProjectService projectService;
+
     @Test
     public void shouldDeleteProject(){
         //given
@@ -27,11 +38,56 @@ public class ProjectServiceTest {
         project.setId(id);
         project.setName("name");
         project.setContent("content");
+        project.setCode("code");
+        project.setDateCreated(new Date());
+        project.setEnabled(true);
         //when
         when(projectRepository.findById(id)).thenReturn(java.util.Optional.of(project));
         projectService.deleteProject(id);
         //then
         verify(projectRepository,times(1)).delete(project);
     }
-
+    @Test
+    public void deleteEnableShouldReturnTrueWhenThereIsNoRelatedIssue(){
+        //given
+        Long id = 1L;
+        Project project = new Project();
+        project.setId(id);
+        project.setName("name");
+        project.setContent("content");
+        project.setCode("code");
+        project.setDateCreated(new Date());
+        project.setEnabled(true);
+        //when
+        when(projectRepository.findById(id)).thenReturn(java.util.Optional.of(project));
+        when(issueService.getAllIssuesByProject(project)).thenReturn(new ArrayList<>());
+        boolean deleteEnable = projectService.deleteEnable(id);
+        //then
+        Assertions.assertTrue(deleteEnable);
+    }
+    @Test
+    public void deleteEnableShouldReturnTrueWhenThereIsRelatedIssue(){
+        //given
+        Long id = 1L;
+        Project project = new Project();
+        project.setId(id);
+        project.setName("name");
+        project.setContent("content");
+        project.setCode("code");
+        project.setDateCreated(new Date());
+        project.setEnabled(true);
+        List<Issue> list = new ArrayList<>();
+        Issue issue = new Issue();
+        issue.setId(id);
+        issue.setTitle("title");
+        issue.setAssignee(new Person());
+        issue.setProject(project);
+        list.add(issue);
+        //when
+        when(projectRepository.findById(id)).thenReturn(java.util.Optional.of(project));
+        when(issueService.getAllIssuesByProject(project)).thenReturn(list);
+        boolean deleteEnable = projectService.deleteEnable(id);
+        //then
+        Assertions.assertFalse(deleteEnable);
+    }
 }
